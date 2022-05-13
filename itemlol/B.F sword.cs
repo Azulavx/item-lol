34c1643b1eed8b693d05a7ca2cd58076f409a6de
3111727 <3111727@TE-GDA-D-D37H14>
2022-05-13 14:43:58 +0200

    Dodaj pliki projektów.

---
+++ itemlol/B.F sword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    public class BFsword : Item
+    {
+        public BFsword()
+        {
+            name = "B.F sword";
+            price = 1300;
+            AddStat(Stat.AD, 40);
+        }
+    }
+}
